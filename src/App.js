ac0b11ab37bbefc815cb8136b0655b048b891582
nayeemhoque109 <mhoqu001@gold.ac.uk>
2024-04-09 23:35:32 +0100

    dashboard start

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import styled from "styled-components";
 import Menu from "./components/menu";
 import Pages from "./components/pages";
@@ -11,11 +12,43 @@ const Container = styled.div`
 
   `
 
+  const Placeholder = styled.div`
+  flex: 3;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 14px;
+  gap: 10px;
+  color: rgba(0, 0, 0, 0.45);
+
+  span {
+    font-size: 32px;
+    color: #525252;
+  }
+`;
+
+const ChatPlaceholder = styled.img`
+  width: 240px;
+  height: 240px;
+  border-radius: 50%;
+  object-fit: contain;
+`;
+
+
 function App() {
+  const [selectedChat, setChat] = useState();
   return (
     <Container>
       <Menu/>
+      {selectedChat ? (
       <Pages/>
+       ):(
+         <Placeholder>
+        <ChatPlaceholder src = "/logo.png"/>fgwefge
+
+        </Placeholder>
+        )}
     </Container>
 
   );
