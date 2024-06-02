f133d4c0fc83e6f69d1bc8bd787aa5505cf19105
nayeemhoque109 <mhoqu001@gold.ac.uk>
2024-06-02 19:23:11 +0100

    gif  deploy 4

--- src/App.js
+++ src/App.js
@@ -172,12 +172,13 @@ function App(props) {
   }
 
   const executeBatFile = () => {
+    alert('Execution has started. Please wait for further messages.');
     fetch('https://yfwl3bva58pv.share.zrok.io/execute', {
       method: 'POST',
     })
     .then(response => response.json())
     .then(data => {
-      setMessage(data.message); // Add this line
+      setMessage(data.message);
       document.getElementById('terminal').innerText = data.output;
     })
     .catch((error) => console.error('Error:', error));
