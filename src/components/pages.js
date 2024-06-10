7823adae29bb5c9aa59e49e169799a91db414920
nayeemhoque109 <mhoqu001@gold.ac.uk>
2024-06-10 18:57:06 +0100

    gif deploy 9.1

--- src/components/pages.js
+++ src/components/pages.js
@@ -116,7 +116,7 @@ const Pages =(props)=>{
     const formData = new FormData();
     formData.append('file', file);
 
-    const response = await fetch('https://d1hk12cjx9vc3u.cloudfront.net/upload', {
+    const response = await fetch('https://www.doc.gold.ac.uk/usr/216/upload', {
       method: 'POST',
       body: formData,
     });
@@ -222,7 +222,7 @@ const Pages =(props)=>{
           <MessageDiv isYours={messageData.senderEmail === userInfo.email}>
             <Message isYours={messageData.senderEmail === userInfo.email}>
               {messageData.text.startsWith('/uploads/') ? (
-          <img src={`https://d1hk12cjx9vc3u.cloudfront.net${messageData.text}`} alt="Uploaded content" />
+          <img src={`https://www.doc.gold.ac.uk/usr/216${messageData.text}`} alt="Uploaded content" />
           ) : (
                 messageData.text
               )}
